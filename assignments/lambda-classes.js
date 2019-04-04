bb32460aef63f953ec848f449a158f3e342b34d3
unknown <bejolo018@gmail.com>
2019-04-04 16:14:02 -0500

    completed objects and ensured all code was functional

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,6 +1,6 @@
 // CODE here for your Lambda Classes
 
-
+//Person Class
 class person {
     constructor(props){
         this.name = props.name;
@@ -14,55 +14,109 @@ class person {
     };
 };
 
-
+//Instructor Class
 class instructor extends person{
-    constructor(props){
+    constructor(props){ 
+        super(props);
         this.specialty = props.specialty;
         this.favLanguage = props.favLanguage;
         this.catchPhrase = props.catchPhrase;
     }
 
     demo(subject){
-        return `Today we are learning about ${this.subject}`
+        return `Today we are learning about ${subject}`
     }
 
     grade(student, subject){
-        `${student.name} receives a perfect score on ${this.subject}`
+        return `${student} receives a perfect score on ${subject}`
     }
 };
 
-
-class students extends person{
+//Student Class
+class student extends person{
     constructor(props){
+        super(props);
         this.previousBackground = props.previousBackground;
         this.className = props.className;
         this.favSubjects = props.favSubjects;
     }
 
     listsSubjects(){
-        return `${favSubjects}`
+        return `${this.favSubjects}`
     }
 
     PRAssignment(subject){
-        `${student.name} has submitted a PR for ${subject}`
+        return `${this.name} has submitted a PR for ${subject}`
     }
 
-    sprintChallenge(){
-        return `${student.name} has begin sprint challenge on ${subject}`
+    sprintChallenge(subject){
+        return `${this.name} has begun sprint challenge on ${subject}`
     }
 };
 
-class projectmanagers extends instructors{
+//Project Manager Class
+class projectmanager extends instructor{
     constructor(props){
+        super(props);
         this.gradClassName = props.gradClassName;
         this.favInstructor = props.favInstructor;
     }
 
     standUp(channel){
-        return `${this.name} announces to ${this.channel} @channel standy times!`
+        return `${this.name} announces to ${channel} @channel standup times!`
     }
 
-    debugsCode(){
-        return `${this.name} debugs ${student.name}'s code on ${this.subject}`
+    debugsCode(subject){
+        return `${this.name} debugs ${student.name}'s code on ${subject}`
     }
 }
+
+//Person Object
+const Ben = new person({
+    name: 'Ben',
+    age: 19,
+    location: 'Texas',
+    gender: 'Male'
+});
+
+//Instructor Object
+const Josh = new instructor({
+    name: 'Josh',
+    age: 35,
+    location: 'Wisconsin',
+    gender: 'Male',
+    speciality: 'Banjo',
+    favLanguage: 'Javascript',
+    catchPhrase: 'I cant spell today'
+});
+
+//Student Object
+const Joe = new student({
+    name: 'Joe',
+    age: 25,
+    location: 'New York',
+    gender: 'Male',
+    previousBackground: 'None',
+    className: 'Web19',
+    favSubjects: 'Javascript IV!'
+})
+
+//Project Manager Object
+const Jamie = new projectmanager({
+    name: 'Jamie',
+    age: 27,
+    location: 'Idaho',
+    gender: 'female',
+    gradClassName: 'Web18',
+    favInstructor: 'Dan'
+})
+
+//Console.logged all methods to ensure they were functional!
+console.log(Ben.speak());
+console.log(Josh.demo('Javascript!'));
+console.log(Josh.grade('Ben', 'Classes!'));
+console.log(Joe.listsSubjects());
+console.log(Joe.PRAssignment('Objects'))
+console.log(Joe.sprintChallenge('Javascript Fundamentals!'))
+console.log(Jamie.standUp('web_sprint'))
+console.log(Jamie.debugsCode('Javascript'))
